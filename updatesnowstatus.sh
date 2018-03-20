d222394be9cc7310679c1e93c2b2176f1ac20b19
kgvprasad <prasad.kgv@cognizant.com>
2018-03-20 21:29:46 +0530

    Update updatesnowstatus.sh

--- updatesnowstatus.sh
+++ updatesnowstatus.sh
@@ -4,12 +4,17 @@ curl -l http://54.165.238.50:8888/job/updatesnow/lastBuild/api/json > jenkin_sta
 result = sed 's/\,/\,\n/g' jenkin_status | grep result | sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'
 sysid = sed 's/\,/\,\n/g' create_incident.txt | grep sys_id | sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'
 
+echo $result
+echo $sysid
+echo $restapiurl
 
 restapiurl="https://kgv06:password-1@cognizantclouddemo.service-now.com/api/now/table/incident/"$sysid
 
 if [ $result == "SUCCESS" ]
 then
+echo "on success"
         curl -H "Content-Type: application/json" -X PUT -d '{"work_notes_list":"SUCCESS AGAIN", "incident_state":"6"}' $restapiurl
 else
+echo "on failure"
         curl -H "Content-Type: application/json" -X PUT -d '{"short_description":"FAILED"}' $restapiurl
 fi
