937de3c5436c74a0830b931072f06d241ae886ff
kgvprasad <prasad.kgv@cognizant.com>
2018-03-20 21:25:58 +0530

    Update updatesnowstatus.sh

--- updatesnowstatus.sh
+++ updatesnowstatus.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+curl -l http://54.165.238.50:8888/job/updatesnow/lastBuild/api/json > jenkin_status
+
 result = sed 's/\,/\,\n/g' jenkin_status | grep result | sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'
 sysid = sed 's/\,/\,\n/g' create_incident.txt | grep sys_id | sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'
-curl -l http://54.165.238.50:8888/job/updatesnow/lastBuild/api/json > jenkin_status
+
 
 restapiurl="https://kgv06:password-1@cognizantclouddemo.service-now.com/api/now/table/incident/"$sysid
 
