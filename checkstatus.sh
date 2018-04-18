039dadbd5f371b1fc40224d84d97fd3cfda26560
kgvprasad <prasad.kgv@cognizant.com>
2018-04-18 16:38:35 +0530

    Update checkstatus.sh

--- checkstatus.sh
+++ checkstatus.sh
@@ -2,7 +2,7 @@
 
 resultfrom=`aws cloudformation describe-stacks --stack-name awscodestar-nodejs-serverle-lambda  | grep StackStatus |sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'`
 echo "STATUS of JENKINS BUILD JOB - " $resultfrom
-
+echo "SYSID= " $1
 restapiurl="https://kgv06:password-1@cognizantclouddemo.service-now.com/api/now/table/incident/"$1
 
 echo "url = " $restapiurl
