34dd9f7ef4c2c68f022fd347301d4ff1c2c80bea
kgvprasad <prasad.kgv@cognizant.com>
2018-05-07 17:17:34 +0530

    Update clistatus.sh

--- clistatus.sh
+++ clistatus.sh
@@ -2,3 +2,13 @@
 aws cloudformation describe-stacks --stack-name drupalstack > temp.txt
 status=`cat temp.txt | grep StackStatus |sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'`
 if [ status = "CREATE_COMPLETE" ]
+
+resultfrom=`aws cloudformation describe-stacks --stack-name drupalstack  | grep StackStatus |sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'`
+echo "STATUS of JENKINS BUILD JOB - " $resultfrom
+
+if [ $resultfrom == CREATE_COMPLETE ]
+then
+echo "http:"`cat temp.txt | grep OutputValue |sed 's/"//g'| cut -d : -f 3 | sed 's/\,//g'`
+else
+echo "yet to handle"
+fi
