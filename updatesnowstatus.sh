1c5a24242ed1fef6fe4c9e4bd05ecde2aea244bb
Jagadt <jagadt@hotmail.com>
2018-05-10 19:03:17 +0530

    Update updatesnowstatus.sh

--- updatesnowstatus.sh
+++ updatesnowstatus.sh
@@ -9,7 +9,7 @@ az group deployment create --resource-group kubernetesrg --name test1 --template
 sleep 10
 
 
-resultfrom=`curl -l http://54.227.32.163:8080/job/AzureShell-ARM%20Deployment/lastBuild/api/json | sed 's/\,/\,\n/g' | grep result | sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'
+resultfrom=`curl -l http://54.227.32.163:8080/job/AzureShell-ARM%20Deployment/lastBuild/api/json | sed 's/\,/\,\n/g' | grep result | sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'`
 echo "STATUS of JENKINS BUILD JOB - " $resultfrom
 
 sleep 10
