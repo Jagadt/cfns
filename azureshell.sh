f8ddd1596d8cf2e07b95f30db7d5eeade17f4abe
Jagadt <jagadt@hotmail.com>
2018-05-10 19:43:10 +0530

    Delete azureshell.sh

--- azureshell.sh
+++
@@ -1,2 +0,0 @@
-/root/bin/az login --service-principal -u 1eef76a0-ff87-4203-8a1f-b7e529fa0b76 -p wExJp5fE8n6/VmCnpXb2iG09tvtnYcX+g5FlQBmMfYU= --tenant b7729f61-f3fd-4583-a00c-05c725eb850b
-/root/bin/az group create --name FrontEndRG3 --location "North Europe"
