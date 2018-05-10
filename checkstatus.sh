51347c0bd71af4a0830309b140cad3aeecb70c4a
Jagadt <jagadt@hotmail.com>
2018-05-10 18:51:03 +0530

    Update checkstatus.sh

--- checkstatus.sh
+++ checkstatus.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-resultfrom=`aws cloudformation describe-stacks --stack-name awscodestar-nodejs-serverle-lambda  | grep StackStatus |sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'`
+#resultfrom=`aws cloudformation describe-stacks --stack-name awscodestar-nodejs-serverle-lambda  | grep StackStatus |sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'`
+
+resultfrom=`curl -l http://54.227.32.163:8080/job/AzureShell-ARM%20Deployment/lastBuild/api/json | sed 's/\,/\,\n/g' | grep result | sed 's/"//g'| cut -d : -f 2 | sed 's/\,//g'
 echo "STATUS of JENKINS BUILD JOB - " $resultfrom
 echo "SYSID= " $1
 restapiurl="https://kgv06:password-1@cognizantclouddemo.service-now.com/api/now/table/incident/"$1
